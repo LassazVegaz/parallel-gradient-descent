95da122e0a18f13d2243aaff82e05d208ecaa5cd
LassazVegaz <lasindunuwanga@gmail.com>
2023-10-05 01:22:03 -0700

    made it happen hardly

--- mpi.c
+++ mpi.c
@@ -8,9 +8,9 @@
 
 #define M 10
 #define N 1000
-#define MAX_ITERATIONS 1000
+#define MAX_ITERATIONS 10000
 #define ALPHA 0.1
-#define ACCURACY_TORLERANCE 0.001
+#define ACCURACY_TORLERANCE 0.0
 
 /// @brief The function we are trying to find coefficients for
 double f(double *x, double *theta)
@@ -81,6 +81,8 @@ int main()
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int localN = N / size;
 
+    printf("size = %d\n", size);
+
     double inputs[N][M];
     double outputs[N];
     double actualTheta[M];
@@ -95,32 +97,80 @@ int main()
             theta[i] = 0;
     }
 
+    double **localInputs = (double **)malloc(sizeof(double *) * localN);
+    for (int i = 0; i < M; i++)
+        localInputs[i] = (double *)malloc(sizeof(double) * M);
+    double *localOutputs = (double *)malloc(sizeof(double) * localN);
+
+    if (rank == 0)
+    {
+        for (int i = 0; i < localN; i++)
+            localInputs[i] = inputs[i];
+
+        for (int i = localN; i < N;)
+        {
+            int upper = i + localN;
+            int dest = i / localN;
+            for (; i < upper; i++)
+            {
+                MPI_Ssend(inputs[i], M, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < localN; i++)
+        {
+            MPI_Status status;
+            MPI_Recv(localInputs[i], M, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+        }
+    }
+    MPI_Scatter(outputs, localN, MPI_DOUBLE, localOutputs, localN, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // printf("Inputs outputs recieved by %d:\n", rank);
+    // for (int _i = 0; _i < localN; _i++)
+    // {
+    //     printf("output - %lf : inputs - ", localOutputs[_i]);
+    //     for (int _j = 0; _j < M; _j++)
+    //         printf("%lf ", localInputs[_i][_j]);
+    //     printf("\n");
+    // }
+
+    // for (int i = 0; i < localN; i++)
+    // {
+    //     if (localOutputs[i] != outputs[i])
+    //         puts("outputs dont match");
+    // }
+    // puts("outputs checking over...");
+
     for (int i = 0; i < MAX_ITERATIONS; i++)
     {
+        // printf("Thetas sending by %d: ", rank);
+        // for (int _i = 0; _i < M; _i++)
+        //     printf("%lf ", theta[_i]);
+        // printf("\n");
+
         MPI_Bcast(theta, M, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        // printf("Thetas recieved by %d: ", rank);
+        // for (int _i = 0; _i < M; _i++)
+        //     printf("%lf ", theta[_i]);
+        // printf("\n");
 
         double newTheta[M];
 
         for (int k = 0; k < M; k++)
         {
-            double *localInputs = (double *)malloc(sizeof(double) * localN);
-            double *localOutputs = (double *)malloc(sizeof(double) * localN);
-            MPI_Scatter(inputs, localN, MPI_DOUBLE, localInputs, localN, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-            MPI_Scatter(inputs, localN, MPI_DOUBLE, localOutputs, localN, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
             double localT = 0;
             for (int n = 0; n < localN; n++)
             {
                 double h = 0;
                 for (int i = 0; i < M; i++)
                 {
-                    h += inputs[n][i] * theta[i];
+                    h += localInputs[n][i] * theta[i];
                 }
-                localT += (h - outputs[n]) * inputs[n][k];
+                localT += (h - localOutputs[n]) * localInputs[n][k];
             }
-
-            free(localInputs);
-            free(localOutputs);
 
             double t = 0;
             MPI_Reduce(&localT, &t, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -128,7 +178,7 @@ int main()
             if (rank == 0)
             {
                 t = theta[k] - ALPHA * localT / N;
-                newTheta[k] = localT;
+                newTheta[k] = t;
             }
         }
 
@@ -138,6 +188,12 @@ int main()
                 theta[i] = newTheta[i];
         }
     }
+
+    if (rank != 0)
+        for (int i = 0; i < M; i++)
+            free(localInputs[i]);
+    free(localInputs);
+    free(localOutputs);
 
     if (rank == 0)
     {
